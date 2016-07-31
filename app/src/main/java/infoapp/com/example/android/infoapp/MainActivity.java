9d824e61665e2ea8e57a757eae4d74de85d30606
S.M Zubair <Devilhunter341@gmail.com>
2016-08-01 01:39:55 +0500

    layout asembling remains

---
+++ app/src/main/java/infoapp/com/example/android/infoapp/MainActivity.java
@@ -0,0 +1,61 @@
+package infoapp.com.example.android.infoapp;
+
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+public void Officalweb (View view){
+    Intent intent = new Intent();
+    intent.setAction(Intent.ACTION_VIEW);
+    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+    intent.setData(Uri.parse("http://www.PORTGRAND.COM"));
+    startActivity(intent);
+}
+    public void Googlemap (View view) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
+        startActivity(intent);}
+
+
+    public void Facebook (View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("http://www.facebook.com/portgrandpakistan"));
+        startActivity(intent);}
+
+    public void twitter (View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("http://www.twitter.com/Portgrand"));
+        startActivity(intent);}
+
+    public void instgram (View view){
+        Uri uri = Uri.parse("http://instagram.com/_u/xxx");
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+        likeIng.setPackage("com.instagram.android");
+
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://instagram.com/pakistan")));
+        }
+    }
+}
+
